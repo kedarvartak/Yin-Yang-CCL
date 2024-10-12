c5abaceec4d2ac5fe350f47ad781edbe91370db5
Kedar Vartak <kedar.vartak22@vit.edu>
2024-10-12 21:39:38 +0530

    updated

--- src/components/card-1.js
+++ src/components/card-1.js
@@ -9,15 +9,19 @@ function Card({ title, description, span = false }) {
     <div
       className={`border-4 border-red-500 p-4 md:p-6 rounded-lg shadow-xl hover:shadow-red-200 transition-shadow duration-300 ${
         span ? 'md:col-span-2' : ''
-      } flex flex-col h-full w-full`}
+      } flex flex-col h-full`}
     >
       {/* Logo and Title Container */}
-      <div className="flex items-center mt-2  md:mb-4">
+      <div className="flex items-center mt-2 md:mb-4">
         {/* Logo Placeholder */}
-        <img src={logo} alt={`${title} Logo`} className="w-10 h-10 md:w-15 md:h-15 mr-2 md:mr-4" />
+        <img
+          src={logo}
+          alt={`${title} Logo`}
+          className="w-10 h-10 md:w-16 md:h-16 mr-2 md:mr-4 flex-shrink-0"
+        />
         {/* Title */}
         <h3
-          className="text-red-500 text-2xl md:text-4xl font-semibold"
+          className="text-red-500 text-3xl md:text-3xl font-semibold"
           style={{ fontFamily: "'Playfair Display', serif" }}
         >
           {title}
@@ -25,7 +29,10 @@ function Card({ title, description, span = false }) {
       </div>
 
       {/* Description */}
-      <p className="text-red-500 text-sm md:text-lg flex-grow" style={{ fontFamily: 'Quicksand' }}>
+      <p
+        className="text-red-500 text-base md:text-lg mt-2 leading-snug break-words"
+        style={{ fontFamily: 'Quicksand' }}
+      >
         {description}
       </p>
     </div>
@@ -36,51 +43,51 @@ export default function UniqueComponent() {
   // Define an array of card data with span specifications
   const cardsData = [
     {
-      title: "Exclusive Teaching",
+      title: 'Exclusive Teaching',
       description:
-        "At YinYang, we focus exclusively on teaching Chinese, ensuring a specialized and immersive learning experience tailored to your needs.  Our curriculum is designed to offer practical, real-world language skills, helping you gain confidence in both spoken and written Chinese.  With personalized lessons and dedicated instructors, we ensure your learning journey is efficient, engaging, and culturally enriching.",
+        "At YinYang, we focus exclusively on teaching Chinese, ensuring a specialized and immersive learning experience tailored to your needs. Our curriculum is designed to offer practical, real-world language skills, helping you gain confidence in both spoken and written Chinese. ",
       span: true, // This card spans two columns on md screens
     },
     {
-      title: "Flexible Sessions",
+      title: 'Flexible Sessions',
       description:
-        "Whether you prefer learning from home or in-person, our sessions are customizable, easily adapted for various levels of proficiency, and offered to suit all schedules.",
+        'Whether you prefer learning from home or in-person, our sessions are customizable, easily adapted for various levels of proficiency, and offered to suit all schedules.',
       span: false,
     },
     {
-      title: "Top-Notch Guidance for Certification",
+      title: 'Top-Notch Guidance for Certification',
       description:
-        "We provide the best guidance to help you achieve international Chinese certification with confidence and professional expertise every step of the way.",
+        'We provide the best guidance to help you achieve international Chinese certification with confidence and professional expertise every step of the way.',
       span: false,
     },
     {
-      title: "Scholarship Assistance",
+      title: 'Scholarship Assistance',
       description:
-        "Our dedicated team offers immediate assistance with Chinese-specific scholarships, opening doors to further education and teaching opportunities.",
+        'Our dedicated team offers immediate assistance with Chinese-specific scholarships, opening doors to further education and teaching opportunities.',
       span: false,
     },
     {
-      title: "Immersive Experience",
+      title: 'Immersive Experience',
       description:
-        "Experience the richness of Chinese culture by attending first-hand immersive sessions that will enrich both language and cultural understanding.",
+        'Experience the richness of Chinese culture by attending first-hand immersive sessions that will enrich both language and cultural understanding.',
       span: false,
     },
     {
-      title: "Professional Translation Services",
+      title: 'Professional Translation Services',
       description:
-        "YinYang offers expert translation services, delivering fast, reliable, and flawless communication in Chinese.",
+        'YinYang offers expert translation services, delivering fast, reliable, and flawless communication in Chinese.',
       span: false,
     },
     {
-      title: "Business Consultancy Expertise",
+      title: 'Business Consultancy Expertise',
       description:
-        "With our profound business consultancy, YinYang helps bridge important cultural gaps, providing market expertise and success in the Chinese market.",
+        'With our profound business consultancy, YinYang helps bridge important cultural gaps, providing market expertise and success in the Chinese market.',
       span: false,
     },
     {
-      title: "Global Perspective",
+      title: 'Global Perspective',
       description:
-        "Broaden your geopolitical understanding by engaging in discussions on current events and trade agreements. This helps deepen your skills in Chinese language and world affairs.  Gain valuable insights into China's role in the global economy, fostering a well-rounded perspective on international relations and commerce.  Stay ahead by mastering the language of one of the world's most influential nations.",
+        "Broaden your geopolitical understanding by engaging in discussions on current events and trade agreements. ",
       span: true, // This card spans two columns on md screens
     },
   ];
@@ -96,7 +103,7 @@ export default function UniqueComponent() {
             style={{ backgroundImage: `url(${pattern})` }}
           ></div>
           <h2
-            className="text-red-500 text-5xl md:text-6xl font-bold mb-2 md:mb-4 leading-tight"
+            className="text-red-500 text-4xl md:text-5xl font-bold mb-2 md:mb-4 leading-tight"
             style={{ fontFamily: "'Playfair Display', serif" }}
           >
             What Makes <span className="text-teal-600">YinYang</span> Unique?
@@ -105,18 +112,17 @@ export default function UniqueComponent() {
             className="text-red-500 mt-4 md:mt-6 text-base md:text-xl font-semibold mb-4 md:mb-8"
             style={{ fontFamily: 'Quicksand' }}
           >
-            YinYang offers unrivalled expertise in Chinese language and culture. From flexible scheduling to top-notch guidance, we provide everything you need for successful application and comprehensive Chinese experiences.
+            YinYang offers unrivalled expertise in Chinese language and culture. From flexible
+            scheduling to top-notch guidance, we provide everything you need for successful
+            application and comprehensive Chinese experiences.
           </p>
-          <div className="flex justify-center">
-            
-          </div>
         </div>
 
         {/* Right Column */}
         <div className="md:col-span-2 relative z-10">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 auto-rows-fr">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 auto-rows-auto">
             {cardsData.map((card, index) => (
-              <Card 
+              <Card
                 key={index}
                 title={card.title}
                 description={card.description}
